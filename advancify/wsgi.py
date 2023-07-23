fab65c915a91ad48dd8fee0d64c6329f40187dc9
qwerty11211 <riyajames807@gmail.com>
2023-07-23 20:01:42 +0530

    Fixing some bugs

--- advancify/wsgi.py
+++ advancify/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for levelup project.
+WSGI config for advancify project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'levelup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advancify.settings')
 
 application = get_wsgi_application()
